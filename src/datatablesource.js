8886552eabd48fc8a3cb1d9a75871ddb319bab79
ahmedosama2022 <ahmedosama2022>
2023-02-13 18:48:37 +0200

    dish

---
+++ src/datatablesource.js
@@ -0,0 +1,121 @@
+export const userColumns = [
+  { field: "id", headerName: "ID", width: 70 },
+  {
+    field: "user",
+    headerName: "User",
+    width: 230,
+    renderCell: (params) => {
+      return (
+        <div className="cellWithImg">
+          <img className="cellImg" src={params.row.img} alt="avatar" />
+          {params.row.username}
+        </div>
+      );
+    },
+  },
+  {
+    field: "email",AhmedosameaderName: "Email",Ahmedosamidth: 230,
+  },
+
+  {
+    field: "age",
+    headerName: "Age",
+    width: 100,
+  },
+  {
+    field: "status",
+    headerName: "Status",
+    width: 160,
+    renderCell: (params) => {
+      return (
+        <div className={`cellWithStatus ${params.row.status}`}>
+          {params.row.status}
+        </div>
+      );
+    },
+  },
+];
+
+//temporary data
+export const userRows = [
+  {
+    id: 1,
+    username: "Ahmed",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    status: "active",
+    email: "1Ahmedosam@gmail.com",
+    age: 35,
+  },
+  {
+    id: 2,
+    username: "osama",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "2Ahmedosam@gmail.com",
+    status: "passive",
+    age: 42,
+  },
+  {
+    id: 3,
+    username: "mohamed",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "3Ahmedosam@gmail.com",
+    status: "pending",
+    age: 45,
+  },
+  {
+    id: 4,
+    username: "Stark",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "4Ahmedosam@gmail.com",
+    status: "active",
+    age: 16,
+  },
+  {
+    id: 5,
+    username: "mona",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "5Ahmedosam@gmail.com",
+    status: "passive",
+    age: 22,
+  },
+  {
+    id: 6,
+    username: "osama",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "6Ahmedosam@gmail.com",
+    status: "active",
+    age: 15,
+  },
+  {
+    id: 7,
+    username: "syed",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "7Ahmedosam@gmail.com",
+    status: "passive",
+    age: 44,
+  },
+  {
+    id: 8,
+    username: "Frances",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "8Ahmedosam@gmail.com",
+    status: "active",
+    age: 36,
+  },
+  {
+    id: 9,
+    username: "Roxie",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "sAhmedosamgmail.com",
+    status: "pending",
+    age: 65,
+  },
+  {
+    id: 10,
+    username: "Roxie",
+    img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    email: "sAhmedosamgmail.com",
+    status: "active",
+    age: 65,
+  },
+];
